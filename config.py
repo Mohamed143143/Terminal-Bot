3b216da9e62b084dbd3fa48a5242099e2a830ba9
Mohamed143143 <fawazkwtq100@gmail.com>
2025-07-07 00:03:29 +0530

    config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ import re
 id_pattern = re.compile(r'^.\d+$')
 
 
-token = os.environ.get("TOKEN")
-app_id = int(os.environ.get("APP_ID"))
-app_hash = os.environ.get("API_HASH")
+token = os.environ.get("8106423376:AAERnS20_8BGEoPbBXuig1oVQDLVbQbzrjY")
+app_id = int(os.environ.get("23107375"))
+app_hash = os.environ.get("29ae7cc52ca0e4c13fd953113743b86b")
 allowed = [int(user) if id_pattern.search(user) else user for user in os.environ.get('AUTH_USERS', '').split()]
 
 help_text = """
